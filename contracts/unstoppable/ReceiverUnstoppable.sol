3d5758b8ec642d08232ef17d9f781c0d54e9b953
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2023-02-03 12:58:21 -0600

    Format unstoppable contracts

--- contracts/unstoppable/ReceiverUnstoppable.sol
+++ contracts/unstoppable/ReceiverUnstoppable.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/interfaces/IERC3156FlashBorrower.sol";
 import "solmate/src/auth/Owned.sol";
-import { UnstoppableVault, ERC20 } from "../unstoppable/UnstoppableVault.sol";
+import {UnstoppableVault, ERC20} from "../unstoppable/UnstoppableVault.sol";
 
 /**
  * @title ReceiverUnstoppable
@@ -25,21 +25,22 @@ contract ReceiverUnstoppable is Owned, IERC3156FlashBorrower {
         uint256 fee,
         bytes calldata
     ) external returns (bytes32) {
-        if (initiator != address(this) || msg.sender != address(pool) || token != address(pool.asset()) || fee != 0)
-            revert UnexpectedFlashLoan();
+        if (
+            initiator != address(this) ||
+            msg.sender != address(pool) ||
+            token != address(pool.asset()) ||
+            fee != 0
+        ) revert UnexpectedFlashLoan();
 
         ERC20(token).approve(address(pool), amount);
 
         return keccak256("IERC3156FlashBorrower.onFlashLoan");
     }
 
-    function executeFlashLoan(uint256 amount) external onlyOwner {
+    function executeFlashLoan(
+        uint256 amount
+    ) external onlyOwner {
         address asset = address(pool.asset());
-        pool.flashLoan(
-            this,
-            asset,
-            amount,
-            bytes("")
-        );
+        pool.flashLoan(this, asset, amount, bytes(""));
     }
 }
