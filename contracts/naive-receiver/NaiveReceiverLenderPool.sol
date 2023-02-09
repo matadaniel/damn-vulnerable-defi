c1dc3e1e211d9224dcb10a3d4fd2c404da1c50f1
Daniel Mata <100956688+matadaniel@users.noreply.github.com>
2023-02-09 16:33:59 -0600

    Format naive-receiver contracts

--- contracts/naive-receiver/NaiveReceiverLenderPool.sol
+++ contracts/naive-receiver/NaiveReceiverLenderPool.sol
@@ -11,26 +11,34 @@ import "./FlashLoanReceiver.sol";
  * @title NaiveReceiverLenderPool
  * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
  */
-contract NaiveReceiverLenderPool is ReentrancyGuard, IERC3156FlashLender {
-
-    address public constant ETH = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+contract NaiveReceiverLenderPool is
+    ReentrancyGuard,
+    IERC3156FlashLender
+{
+    address public constant ETH =
+        0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
     uint256 private constant FIXED_FEE = 1 ether; // not the cheapest flash loan
-    bytes32 private constant CALLBACK_SUCCESS = keccak256("ERC3156FlashBorrower.onFlashLoan");
+    bytes32 private constant CALLBACK_SUCCESS =
+        keccak256("ERC3156FlashBorrower.onFlashLoan");
 
     error RepayFailed();
     error UnsupportedCurrency();
     error CallbackFailed();
 
-    function maxFlashLoan(address token) external view returns (uint256) {
+    function maxFlashLoan(
+        address token
+    ) external view returns (uint256) {
         if (token == ETH) {
             return address(this).balance;
         }
         return 0;
     }
 
-    function flashFee(address token, uint256) external pure returns (uint256) {
-        if (token != ETH)
-            revert UnsupportedCurrency();
+    function flashFee(
+        address token,
+        uint256
+    ) external pure returns (uint256) {
+        if (token != ETH) revert UnsupportedCurrency();
         return FIXED_FEE;
     }
 
@@ -40,20 +48,24 @@ contract NaiveReceiverLenderPool is ReentrancyGuard, IERC3156FlashLender {
         uint256 amount,
         bytes calldata data
     ) external returns (bool) {
-        if (token != ETH)
-            revert UnsupportedCurrency();
-        
+        if (token != ETH) revert UnsupportedCurrency();
+
         uint256 balanceBefore = address(this).balance;
 
         // Transfer ETH and handle control to receiver
-        SafeTransferLib.safeTransferETH(address(receiver), amount);
-        if(receiver.onFlashLoan(
-            msg.sender,
-            ETH,
-            amount,
-            FIXED_FEE,
-            data
-        ) != CALLBACK_SUCCESS) {
+        SafeTransferLib.safeTransferETH(
+            address(receiver),
+            amount
+        );
+        if (
+            receiver.onFlashLoan(
+                msg.sender,
+                ETH,
+                amount,
+                FIXED_FEE,
+                data
+            ) != CALLBACK_SUCCESS
+        ) {
             revert CallbackFailed();
         }
 
